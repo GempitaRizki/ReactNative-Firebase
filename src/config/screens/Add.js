7c3b265610e7fffec3e11aa68749c2dc32b39cda
Gempita Rizki (R) ? <38187462+GempitaRizki@users.noreply.github.com>
2022-10-27 11:07:21 +0700

    add to use state

--- src/config/screens/Add.js
+++ src/config/screens/Add.js
@@ -1,10 +1,33 @@
 import * as React from 'react'
 import * as ReactNative from 'react-native'
+import EmojiPicker from 'rn-emoji-keyboard'
 
 export default function Home() {
+    const [isOpen, setIsOpen] = React.useState(true)
+    const [ newItem, setNewItem] = React.useState({
+        emoji: '😎',
+        name : '',
+        price: 0,
+        isSold: false,
+        createAt: new Date()
+    })
+
+    const handlePick = (emojiObject) => {
+        setNewItem({
+            ...newItem,
+            emoji: emojiObject.emoji
+        })
+    }
+
     return (
         <ReactNative.View style={styles.container}>
-            <ReactNative.Text>Ini adalah halaman Add </ReactNative.Text>
+            <ReactNative.Text>Jual Product Baru</ReactNative.Text>
+            <ReactNative.Text>{newItem.emoji}</ReactNative.Text>
+            <EmojiPicker
+                onEmojiSelected={handlePick}
+                open={isOpen}
+                onClose={() => setIsOpen(false)}
+            />
         </ReactNative.View>
     )
 }
