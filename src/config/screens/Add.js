b0eb23857e91e1b838f28832cf44c7602f1ed083
Gempita Rizki (R) ? <38187462+GempitaRizki@users.noreply.github.com>
2022-10-31 10:49:56 +0700

    Getting TypeError: undefined

--- src/config/screens/Add.js
+++ src/config/screens/Add.js
@@ -1,12 +1,26 @@
 import * as React from 'react'
 import * as ReactNative from 'react-native'
-import { useNavigation } from '@react-navigation/native'
+import * as EmojiPicker from 'rn-emoji-keyboard'
 
 export default function Add() {
-    const navigation = useNavigation()
+    const [ isOpen, setIsOpen ] = React.useState(true) 
+    const [ newItem, setNewItem ] = React.useState({
+        emoji: '👾',
+        name: '',
+        harga: 0,
+        terjual: false,
+        createAt: new Date(),
+    })
     return (
         <ReactNative.View style={styles.container}>
+            
             <ReactNative.Text>Ini adalah halaman Add </ReactNative.Text>
+            <ReactNative.Text>{newItem.emoji}</ReactNative.Text>
+            <EmojiPicker
+                onEmojiSelected={handlePick}
+                open={isOpen}
+                onClose={() => setIsOpen(false)}
+            />
         </ReactNative.View>  
     )
 }
